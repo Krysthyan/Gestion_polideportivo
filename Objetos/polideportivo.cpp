1fdee5063aac2a126ebc340adf474f5980909fd9
Krysthyan <krysthyan_09h@yahoo.com>
2016-07-18 16:16:38 -0500

    nuevo

--- Objetos/polideportivo.cpp
+++ Objetos/polideportivo.cpp
@@ -2,6 +2,13 @@
 
 Polideportivo *Polideportivo::instancia = 0 ;
 
+Polideportivo *Polideportivo::instance(std::__cxx11::string nombre, std::__cxx11::string descripcion)
+{
+    if(instancia == 0)
+        instancia = new Polideportivo(nombre, decripcion);
+    return instancia;
+}
+
 Polideportivo *Polideportivo::instance()
 {
     if(instancia == 0)
@@ -9,7 +16,19 @@ Polideportivo *Polideportivo::instance()
     return instancia;
 }
 
-Polideportivo::Polideportivo()
+void Polideportivo::agregar_espacio(Espacio_comun espacio_comun)
+{
+    this->espacios_deportivos.push_back(espacio_comun);
+}
+
+void Polideportivo::eliminar_espacio(Espacio_comun espacio_comun)
 {
 
+}
+
+Polideportivo::Polideportivo(std::__cxx11::string nombre,
+                             std::__cxx11::string descripcion)
+{
+    this->nombre = nombre;
+    this->descripcion = descripcion;
 }
